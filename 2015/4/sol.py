3f41e5b108db4d914804a8a75ec42a6fd2ce3586
Tyrus Tenneson <ttenneson@gmail.com>
2015-12-14 22:12:27 -0800

    Day 4 solved.

---
+++ 2015/4/sol.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+'''
+Advent of Code 2015
+Day 3
+
+By Tyrus Tenneson
+2015-12-14
+'''
+import hashlib
+import os
+import sys
+
+
+def solve_one(problem):
+    n = 0
+    hash = '01234'
+    while hash[:5] != '00000':
+        n += 1
+        hash = hashlib.md5(problem + str(n)).hexdigest()
+    return n
+
+def solve_two(problem):
+    n = 0
+    hash = '012345'
+    while hash[:6] != '000000':
+        n += 1
+        hash = hashlib.md5(problem + str(n)).hexdigest()
+    return n
+
+
+def parse_problem(f):
+    return f.read().strip()
+
+
+def print_solutions():
+    f = open(os.path.join(sys.path[0], 'input'), 'r')
+    problem = parse_problem(f)
+    print solve_one(problem)
+    print solve_two(problem)
+
+
+if __name__ == '__main__':
+    print_solutions()
